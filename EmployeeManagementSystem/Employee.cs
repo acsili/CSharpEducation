9f940c21d2928e8d32247c01bf292444add84f8a
acsili <ilyaovchinnikov4@gmail.com>
2024-09-13 17:40:08 +0400

    initial EmployeeManagementSystem

---
+++ EmployeeManagementSystem/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+  /// <summary>
+  /// Сотрудник.
+  /// </summary>
+  public abstract class Employee
+  {
+    /// <summary>
+    /// Имя.
+    /// </summary>
+    public abstract string Name { get; set; }
+
+    /// <summary>
+    /// Зарплата.
+    /// </summary>
+    public abstract decimal BaseSalary { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract decimal CalculateSalary();
+  }
+}
---
+++ EmployeeManagementSystem/PartTimeEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+  /// <summary>
+  /// Частичный сотрудник.
+  /// </summary>
+  public class PartTimeEmployee : Employee
+  {
+    public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override decimal BaseSalary { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override decimal CalculateSalary()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
