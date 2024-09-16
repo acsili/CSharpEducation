e6a73893adcae1b2958ef7b53a41f19390467b1f
acsili <ilyaovchinnikov4@gmail.com>
2024-09-16 20:32:27 +0400

    Initial UserManagementSystem

---
+++ UserManagementSystem/User.cs
@@ -0,0 +1,45 @@
+﻿
+namespace UserManagementSystem
+{
+  /// <summary>
+  /// Пользователь.
+  /// </summary>
+  public class User
+  {
+    #region Поля и свойства
+
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Имя.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Почтовый адрес.
+    /// </summary>
+    public string Email { get; set; }
+
+    #endregion
+
+    #region Конструкторы
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="id">Идентификатор.</param>
+    /// <param name="name">Имя.</param>
+    /// <param name="email">Почтовый адрес.</param>
+    public User(int id, string name, string email)
+    {
+      Id = id;
+      Name = name;
+      Email = email;
+    }
+
+    #endregion
+  }
+}
---
+++ UserManagementSystem/UserManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem
+{
+  /// <summary>
+  /// Репозиторий для работы с пользователями.
+  /// </summary>
+  public class UserManager
+  {
+    #region Поля и свойства
+
+    /// <summary>
+    /// Пользователи.
+    /// </summary>
+    public List<User> Users = new();
+
+    #endregion
+
+    #region Методы
+
+    public void AddUser(User user)
+    {
+
+    }
+
+    public void RemoveUser(int id)
+    {
+
+    }
+
+    public User GetUser(int id)
+    {
+      return Users.FirstOrDefault(x => x.Id == id);
+    }
+
+
+
+    #endregion
+  }
+}
