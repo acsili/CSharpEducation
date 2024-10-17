b719c52c1fb5a56dc2c630cf2f7993876c10b348
acsili <ilyaovchinnikov4@gmail.com>
2024-10-17 15:40:59 +0400

    Added TestCase Attributes

--- Phonebook.Tests/PhoneNumberValidatorTests.cs
+++ Phonebook.Tests/PhoneNumberValidatorTests.cs
@@ -7,58 +7,61 @@ namespace Phonebook.Tests
   [TestFixture]
   public class PhoneNumberValidatorTests
   {
-    private List<PhoneNumber> validNumbers;
-    private List<PhoneNumber> invalidNumbers;
 
     [OneTimeSetUp]
     public void OneTimeSetup()
     {
-      validNumbers = new List<PhoneNumber>
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+    }
+
+    [Test]
+    [TestCase("+1 (123) 456-7890", PhoneNumberType.Personal)]
+    [TestCase("+44 (204) 794-0958", PhoneNumberType.Work)]
+    [TestCase("+7 (495) 123-4567", PhoneNumberType.Personal)]
+    public void Validate_ValidPhoneNumbers_ShouldNotThrowException(string number, PhoneNumberType type)
+    {
+      // Arrange
+      var phoneNumber = new PhoneNumber(number, type);
+
+      // Act 
+      void result() => PhoneNumberValidator.Validate(phoneNumber);
+
+      // Assert
+      Assert.DoesNotThrow(result);
+    }
+
+    [Test]
+    [TestCase("1234567890", PhoneNumberType.Personal)]
+    [TestCase("+1 123 456 7890", PhoneNumberType.Work)]
+    [TestCase("+1 (123) 456-78", PhoneNumberType.Personal)]
+    [TestCase("+1 (123) 456-78901", PhoneNumberType.Personal)]
+    public void Validate_InvalidPhoneNumbers_ShouldThrowException(string number, PhoneNumberType type)
+    {
+      // Arrange
+      var phoneNumber = new PhoneNumber(number, type);
+
+      // Act 
+      void result() => PhoneNumberValidator.Validate(phoneNumber);
+
+      // Assert
+      Assert.Throws<ArgumentException>(result);
+    }
+
+    [Test]
+    public void ValidateList_ValidPhoneNumbers_ShouldNotThrowException()
+    {
+      // Arrange
+      var validNumbers = new List<PhoneNumber>
       {
         new PhoneNumber("+1 (123) 456-7890", PhoneNumberType.Personal),
         new PhoneNumber("+44 (204) 794-0958", PhoneNumberType.Work),
         new PhoneNumber("+7 (495) 123-4567", PhoneNumberType.Personal)
       };
 
-      invalidNumbers = new List<PhoneNumber>
-      {
-        new PhoneNumber("1234567890", PhoneNumberType.Work),
-        new PhoneNumber("+1 123 456 7890", PhoneNumberType.Personal),
-        new PhoneNumber("+1 (123) 456-78", PhoneNumberType.Work),
-        new PhoneNumber("+1 (123) 456-78901", PhoneNumberType.Personal)
-      };
-    }
-
-    [OneTimeTearDown]
-    public void OneTimeTearDown()
-    {
-      validNumbers = null;
-      invalidNumbers = null;
-    }
-
-    [Test]
-    public void Validate_ValidPhoneNumbers_ShouldNotThrowException()
-    {
-      // Assert
-      foreach (var number in validNumbers)
-      {
-        Assert.DoesNotThrow(() => PhoneNumberValidator.Validate(number));
-      }
-    }
-
-    [Test]
-    public void Validate_InvalidPhoneNumbers_ShouldThrowException()
-    {
-      // Assert
-      foreach (var number in invalidNumbers)
-      {
-        Assert.Throws<ArgumentException>(() => PhoneNumberValidator.Validate(number));
-      }
-    }
-
-    [Test]
-    public void ValidateList_ValidPhoneNumbers_ShouldNotThrowException()
-    {
       // Assert
       Assert.DoesNotThrow(() => PhoneNumberValidator.ValidateList(validNumbers));
     }
@@ -67,13 +70,16 @@ namespace Phonebook.Tests
     public void ValidateList_InvalidPhoneNumbers_ShouldThrowException()
     {
       //  Arrange
-      var mixedNumbers = new List<PhoneNumber>(validNumbers);
-
-      // Act
-      mixedNumbers.AddRange(invalidNumbers);
+      var invalidNumbers = new List<PhoneNumber>
+      {
+        new PhoneNumber("1234567890", PhoneNumberType.Work),
+        new PhoneNumber("+1 123 456 7890", PhoneNumberType.Personal),
+        new PhoneNumber("+1 (123) 456-78", PhoneNumberType.Work),
+        new PhoneNumber("+1 (123) 456-78901", PhoneNumberType.Personal)
+      };
 
       // Assert
-      Assert.Throws<ArgumentException>(() => PhoneNumberValidator.ValidateList(mixedNumbers));
+      Assert.Throws<ArgumentException>(() => PhoneNumberValidator.ValidateList(invalidNumbers));
     }
   }
 }
